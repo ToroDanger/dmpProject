7cd2fa1e3a8da41a1eb9c11b70a5cdcfd0617869
eritoD <eri.diaz@duocuc.cl>
2024-06-24 22:10:19 -0400

    inicio normal

--- dmp/urls.py
+++ dmp/urls.py
@@ -19,5 +19,6 @@ from django.urls import path ,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('modelo/', include('modelo.urls')),
+    path('', include('modelo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
